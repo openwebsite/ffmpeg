20156fad1598b21ae9c3e0c8e7c6f235815af1c8
elesos <hnrayer@gmail.com>
2020-05-09 14:05:52 +0800

    first blood

--- generate-doc.sh
+++ generate-doc.sh
@@ -9,9 +9,7 @@ die() {
     exit 1
 }
 
-#if [ $# != 1 ]; then
-#    die "Usage: $0 <ffmpeg-source>"
-#fi
+
 
 
 current_dir=$(pwd)
@@ -23,11 +21,8 @@ export FFMPEG_HEADER3="$(cat src/template_head3)"
 export FFMPEG_FOOTER="$(cat src/template_footer1 src/template_footer_prod src/template_footer2)"
 export FA_ICONS=true
 
-rm -rf build-doc
-mkdir build-doc && cd build-doc
-$current_dir/ffmpeg-4.2.2/configure --enable-gpl --disable-x86asm --enable-htmlpages|| die "configure failed"
+
+cd $current_dir/ffmpeg-4.2.2
+./configure --enable-gpl --disable-x86asm --enable-htmlpages || die "configure failed"
 make doc || die "doc not made"
-#cp doc/*.html ../docs/ || die "copy failed"
-cp $current_dir/ffmpeg-4.2.2/doc/*.html ../docs/ || die "copy failed"
-#cd ..
-#rm -rf build-doc
+cp $current_dir/ffmpeg-4.2.2/doc/*.html $current_dir/docs/ || die "copy failed"
