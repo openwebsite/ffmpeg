8d1ef2e21c8da3f8fe7c43061e32fc37f4430a18
elesos <hnrayer@gmail.com>
2020-05-09 10:19:56 +0800

    first blood

--- generate-doc.sh
+++ generate-doc.sh
@@ -22,9 +22,9 @@ export FFMPEG_HEADER3="$(cat src/template_head3)"
 export FFMPEG_FOOTER="$(cat src/template_footer1 src/template_footer_prod src/template_footer2)"
 export FA_ICONS=true
 
-#rm -rf build-doc
-#mkdir build-doc && cd build-doc
-$src/configure --enable-gpl --disable-x86asm || die "configure failed"
+rm -rf build-doc
+mkdir build-doc && cd build-doc
+$current_dir/configure --enable-gpl --disable-x86asm || die "configure failed"
 make doc || die "doc not made"
 cp doc/*.html ../docs/ || die "copy failed"
 
